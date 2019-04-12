6e84ad9a5ab82848550bfcd4a675bcda5e71e295
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-04-12 18:46:11 -0400

    add react testing librabry, test render function

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render } from 'react-testing-library';
+
 import App from './App';
 
-it('renders without crashing', () => {
+xit('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('app renders without crashing', () => {
+  render(<App />);
+})
